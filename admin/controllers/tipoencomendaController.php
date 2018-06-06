cbbcaad792a7a90772907cf1cff0e0d30ceb9c05
Carlos Eduardo <carlos.contato00@gmail.com>
2018-06-05 22:28:32 -0300

    Redirecionamento de usuario para login

--- admin/controllers/tipoencomendaController.php
+++ admin/controllers/tipoencomendaController.php
@@ -6,6 +6,13 @@ class tipoencomendaController extends controller {
 		if (!$usuario->logado()) { //valida o retorno do método se ele é true ou false.
 			header('Location: '.URL.'/login');
 		}
+		if ($usuario = '2'){
+			unset($_SESSION['id']); //Destroi a SESSION ID.
+			unset($_SESSION['login']); //Destroi a SESSION.
+			unset($_SESSION['senha']); //Destroi a SESSION.
+			session_destroy();
+			header('Location: '.URL.'/login');
+		}
 	}
 
 	public function index(){
